9ce915beda144716579a168e6ba3cf8cd4b8c818
Manish Pritmani <manish.pritmani06@gmail.com>
2024-10-13 01:43:41 +0530

    Added Logs in Receiver

--- app/src/main/java/com/manish/androidhandson/broadcast_receiver/CustomReceiver.kt
+++ app/src/main/java/com/manish/androidhandson/broadcast_receiver/CustomReceiver.kt
@@ -3,9 +3,10 @@ package com.manish.androidhandson.broadcast_receiver
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import timber.log.Timber
 
 class CustomReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context?, intent: Intent?) {
-
+        Timber.d("onReceive called!")
     }
 }
